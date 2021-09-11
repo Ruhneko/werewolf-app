1f88fa4eabbc0d94c99b6629a22ad3e08056a3bb
Ruhneko <nicholas_custodio@dlsu.edu.ph>
2021-09-11 17:09:21 +0800

    Revert "Cards in playarea are not defined ☠"
    This reverts commit 056c4acdf8981320fb6a1e05b6f56cc9902930c0.

--- src/components/play-area/PlayArea.js
+++ src/components/play-area/PlayArea.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react'
 import parse from 'html-react-parser';
 import { Socket } from 'socket.io-client';
-import PlayerCard from './PlayerCard';
 
 export default class PlayArea extends Component {
 
     render() {
         const {socket, user, connectedUsers} = this.props
-        let Cards = Object.keys(connectedUsers).map((key) =>{
-            return ( <PlayerCard user={connectedUsers[key] }/>
-               
-            )}      
-         )
-        
-        console.log(Cards)
-
+        console.log(connectedUsers)
         return(
             <div className="playArea">
                 <div className="god">PLAYER WAKE UP</div>
                 <div className="playArea-row">
-                 { Cards }                    
-                    {/* <div className="player-card">
+                    <div className="player-card">
                         <div className="player-card content">USERNAME</div>
                         <button className="vote-button">Vote</button>
                     </div>
@@ -60,7 +51,7 @@ export default class PlayArea extends Component {
                     <div className="player-card">
                         <div className="player-card content">Werewolf</div>
                         <button className="vote-button">Vote</button>
-                    </div> */}
+                    </div>
                 </div>        
                       
             </div>
