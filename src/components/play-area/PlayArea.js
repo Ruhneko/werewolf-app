0a3d3b82a62b6e21138ba455eed897a17acef1ca
Ruhneko <nicholas_custodio@dlsu.edu.ph>
2021-09-11 19:23:07 +0800

    player turn

--- src/components/play-area/PlayArea.js
+++ src/components/play-area/PlayArea.js
@@ -5,10 +5,35 @@ import PlayerCard from './PlayerCard';
 
 export default class PlayArea extends Component {
 
+    /*
+        static ROLE_WEREWOLF = "ROLE_WEREWOLF";
+        static ROLE_SEER = "ROLE_SEER";
+        static ROLE_ROBBER = "ROLE_ROBBER";
+        static ROLE_TROUBLEMAKER = "ROLE_TROUBLEMAKER";
+        static ROLE_VILLAGER = "ROLE_VILLAGER";
+    */
+    constructor(props){
+        super(props)
+
+        this.state = {
+            turn: "",
+        }
+    }
+
+    componentDidMount(){
+        const {socket} = this.props
+
+        socket.on(CHANGE_TURN, (turn)=>{
+            this.setState({turn})
+        }) 
+    }
+
     render() {
+
+        
         const {socket, user, connectedUsers} = this.props
         let Cards = Object.keys(connectedUsers).map(key=> {
-            return (<PlayerCard user={connectedUsers[key]} />)
+            return (<PlayerCard turn = {turn} user={connectedUsers[key]} />)
         })
 
         console.log(Cards)
