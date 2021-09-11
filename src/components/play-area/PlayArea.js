4ed638c592deb46bdb5428fd1d61de4332c76346
Ruhneko <nicholas_custodio@dlsu.edu.ph>
2021-09-11 17:08:28 +0800

    dynamic cards

--- src/components/play-area/PlayArea.js
+++ src/components/play-area/PlayArea.js
@@ -1,61 +1,25 @@
 import React, { Component } from 'react'
 import parse from 'html-react-parser';
 import { Socket } from 'socket.io-client';
+import PlayerCard from './PlayerCard';
 
 export default class PlayArea extends Component {
 
     render() {
         const {socket, user, connectedUsers} = this.props
-        console.log(connectedUsers)
+        let Cards = Object.keys(connectedUsers).map(key=> {
+            return (<PlayerCard user={connectedUsers[key]} />)
+        })
+
+        console.log(Cards)
+
         return(
             <div className="playArea">
                 <div className="god">PLAYER WAKE UP</div>
                 <div className="playArea-row">
-                    <div className="player-card">
-                        <div className="player-card content">USERNAME</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-                </div>  
-
-                    <div className="playArea-row">
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-
-                    <div className="player-card">
-                        <div className="player-card content">Werewolf</div>
-                        <button className="vote-button">Vote</button>
-                    </div>
-                </div>        
-                      
+                   {Cards}
+                </div>           
             </div>
-            
         )
     }
 }
